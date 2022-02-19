7da9c61282f0e624b33a2e1e7a10190d3f5f8846
Ben Kelly <Benjamin.J.Kelly@live.com>
2022-02-19 18:46:54 -0500

    Write a function that accomplishes model scoring, and a function that will deploy the model

--- deployment.py
+++ deployment.py
@@ -16,12 +16,25 @@ with open('config.json','r') as f:
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
-
+model_path = os.path.join(config['output_model_path'])
+output_folder_path = config['output_folder_path']
 
 ####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
+def store_model_into_pickle():
+    cur_path = os.getcwd()
+    old_loc_model = cur_path + "/" + model_path + "/" + 'trainedmodel.pkl'
+    new_loc_model = cur_path + "/" + prod_deployment_path + "/" + 'trainedmodel.pkl'
+
+    old_loc_model_score = cur_path + "/" + model_path + "/" + "latestscore.txt"
+    new_loc_model_score = cur_path + "/" + prod_deployment_path + "/" + "latestscore.txt"
+
+    old_ingested_files = cur_path + "/" + output_folder_path + "/" + "ingestedfiles.txt"
+    new_ingested_files = cur_path + "/" + prod_deployment_path + "/" + "ingestedfiles.txt"
+    # copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
+    os.rename(old_loc_model,new_loc_model)
+    os.rename(old_loc_model_score, new_loc_model_score)
+    os.rename(old_ingested_files, new_ingested_files)
         
-        
-        
+if __name__ == '__main__':
+    store_model_into_pickle()
 
--- scoring.py
+++ scoring.py
@@ -8,18 +8,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
-
-
 #################Load config.json and get path variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
-
+test_data_path = os.path.join(config['test_data_path'])
+model_path = os.path.join(config['output_model_path'])
 
 #################Function for model scoring
 def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
-    #it should write the result to the latestscore.txt file
+    cur_path = os.getcwd()
+    model = LogisticRegression()
+    # load saved model
+    with open(cur_path + "/" + model_path + "/" + 'trainedmodel.pkl', 'rb') as f:
+        model = pickle.load(f)
+    X = np.empty
+    y = np.empty
+    for f in os.listdir(test_data_path):
+        if(f.endswith('csv')):
+            X = np.loadtxt(cur_path + "/" + test_data_path + "/" + f, delimiter=',', skiprows=1, usecols=[1,2,3])
+            y = np.loadtxt(cur_path + "/" + test_data_path + "/" + f, delimiter=',', skiprows=1, usecols=[4])
 
+    predictions = model.predict(X)
+
+    latest = open(cur_path + "/" + model_path + "/" + "latestscore.txt", "w")
+    latest.write('F1 is: ' + str(metrics.f1_score(y, predictions)) + "\n")
+    latest.close()
+
+if __name__ == '__main__':
+    score_model()
