151306b7264d9910e9e78ae5e8b51b641a34d173
Ben Kelly <Benjamin.J.Kelly@live.com>
2022-02-27 14:42:27 -0500

    Combine these API outputs and write the combined outputs to api_returns.txt

--- apicalls.py
+++ apicalls.py
@@ -1,22 +1,27 @@
 import requests
+import json
+import os
 
 #Specify a URL that resolves to your workspace
 URL = "http://127.0.0.1/"
 
+with open('config.json','r') as f:
+    config = json.load(f)
 
+model_path = os.path.join(config['output_model_path'])
 
 #Call each API endpoint and store the responses
 pth = {'path': '/home/kelbenj/Udacity/MachineLearningDevOps/Client_Turnover_Risk_Assessment/testdata'}
-response1 = requests.post('http://0.0.0.0:8000/prediction', data=pth).text
-response2 = requests.get('http://0.0.0.0:8000/scoring').text
-response3 = requests.get('http://0.0.0.0:8000/summarystats').text
-response4 = requests.get('http://0.0.0.0:8000/diagnostics').text
+response1 = requests.post('http://0.0.0.0:8000/prediction', data=pth).content.decode('utf8')
+response2 = requests.get('http://0.0.0.0:8000/scoring').content.decode('utf8')
+response3 = requests.get('http://0.0.0.0:8000/summarystats').content.decode('utf8')
+response4 = requests.get('http://0.0.0.0:8000/diagnostics').content.decode('utf8')
 
 #combine all API responses
-responses = response1 + response2 + response3 + response4
+responses = "\n".join([response1 + response2 + response3 + response4])
 
 print(responses)
 
 
-
-
+with open(os.path.join(model_path, "api_returns.txt"), "a+") as f:
+    f.write(responses)
